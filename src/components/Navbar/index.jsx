e5965bada369bbf62a62ae03c723c8df65ddd2fb
moelshenawy <mohamedgk5@gmail.com>
2024-08-23 00:57:49 +0300

    Add video tagging feature and update styles

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -10,16 +10,14 @@ const Navbar = () => {
       <div className="container">
         <div className={styles.sec_container}>
           <div className={styles.logo}>
-            <img src="/assets/imgs/white_logo.png" alt="logo this is islam" />
+            <img src="https://suwa.com.sa/assets/images/logo-light.png" alt="logo this is islam" />
           </div>
 
           <div className={styles.settings_container}>
             <div className={styles.icon_container}>
               <GiHamburgerMenu />
             </div>
-            <div className={styles.icon_container}>
-              <MdLanguage />
-            </div>
+
           </div>
         </div>
       </div >
